0f2072dc6e8d60ea1f19fd164e3cbafaf394adcd
PurpleLobster31 <lopesmatheus642@gmail.com>
2022-08-16 17:57:12 -0300

    Aula 3 - Não Terminada

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
-print(url)
+#url = "https://bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
+url = " "
+
+#Sanitização da URL
+url = url.replace(" ", "")
+
+#validação da URL
+if (url==""):
+    raise ValueError("A URL está vazia.")
 
 '''
 fatiamento:
@@ -16,14 +23,16 @@ url_parametros = url[indexInterrog+1:]
 print(url_parametros)
 
 # Busca o valor de um parâmetro
-parametro_busca = 'moedaOrigem'
+parametro_busca = 'quantidade'
 indice_parametro = url_parametros.find(parametro_busca)    
 tamanho_parametro = len(parametro_busca)
 indice_valor = indice_parametro + tamanho_parametro + 1
 indice_e_comercial = url_parametros.find('&', indice_valor)
+
 if (indice_e_comercial == -1):
     valor = url_parametros[indice_valor:]
 else:
     valor = url_parametros[indice_valor:indice_e_comercial]
+    
 print(valor)
 
