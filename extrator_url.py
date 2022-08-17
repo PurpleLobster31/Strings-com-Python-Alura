6d7cca5d04d64e0cf3f11b77ad65910f384d78f4
PurpleLobster31 <lopesmatheus642@gmail.com>
2022-08-17 18:13:24 -0300

    Aula 3 e 4

--- extrator_url.py
+++ extrator_url.py
@@ -1,14 +1,23 @@
+import re
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
         self.valida_url()
         
     def sanitiza_url(self, url):
-        return url.strip()
+        if (type(url) == str):
+            return url.strip()
+        else:
+            return ""
     
     def valida_url(self):
-        if self.url == "":
+        if not self.url:
             raise ValueError("A URL está vazia.")
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(self.url)
+        if not match:
+            raise ValueError("A URL não é válida")
         
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
@@ -31,6 +40,6 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
-extrator_url = ExtratorURL("https://bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real")
+extrator_url = ExtratorURL("https://bytebank.com/cambioo?quantidade=100&moedaDestino=dolar&moedaOrigem=real")
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
